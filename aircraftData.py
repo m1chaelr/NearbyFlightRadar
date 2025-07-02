db8aef1b61fdc949f08a0d08237af437c7fc4021
m1chaelrQUT <n11278633@qut.edu.au>
2025-07-02 11:09:35 +1000

    Refactor: aircraft data loading logic and improve error handling

--- aircraftData.py
+++ aircraftData.py
@@ -1,14 +1,29 @@
 # This file is used to load aircraft data from a CSV file into a dictionary. 
 # It reads the CSV file and creates a mapping of ICAO24 codes to aircraft models.
 import csv
+import sys
 
 def load_aircraft_data(csv_path: str) -> dict:
     """Load aircraft data from a CSV file into a dictionary."""
+
+    csv.field_size_limit(sys.maxsize) # Increase the field size limit to handle large CSV file
     aircraft_data = {}
-    with open(csv_path, newline = '', encoding = 'utf-8') as file:
+    with open(csv_path, newline = '') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            icao24 = row["'icao24'"].strip()
-            model = row['model'].strip() if row['model'] else "Unkown"
-            aircraft_data[icao24] = model
+            try:
+                # Clean up keys by stripping single quotes & removing None values
+                cleaned_row = {(key.strip("'") if key else "") : (value.strip("'") if value else "")
+                            for key, value in row.items()
+                            if key is not None}
+                
+                # Read the ICAO24 code and airplane model
+                icao24 = cleaned_row['icao24'].strip().lower()
+                model = cleaned_row['model'].strip() if cleaned_row['model'] else "Unknown"
+
+                # Store in the dictionary for lookup
+                aircraft_data[icao24] = model
+
+            except Exception as e:
+                print(f"Error processing row {row}: {e}")
     return aircraft_data
--- main.py
+++ main.py
@@ -3,7 +3,13 @@
 # It is responsible for tracking flight information and providing updates.
 
 from aircraftData import load_aircraft_data  # Import the function to load aircraft data from a CSV file
-from callOpenSkyRest import getBoxData
+# from callOpenSkyRest import getBoxData
 
 aircraft_models = load_aircraft_data('aircraftDataset.csv')  # Load aircraft data from the CSV file
 getBoxData(45.8389, 5.9962, 47.8229, 10.5226)  # Example coordinates for the bounding box
+
+print(list(aircraft_models.items())[:10])
+
+
+# icao_code = "06a142"  # Example ICAO24 code to look up
+# model = aircraft_models.get(icao_code.strip().lower(), "Unknown")  # Show the aircraft model for the given ICA
